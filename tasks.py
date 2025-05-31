9fc2dc4c2918802d6c860763860ae0475df7ab82
James Hatfield <james.hatfield17@icloud.com>
2025-05-31 12:40:38 +0100

    Create task class and create initial methods, add psedocode for definitions

--- tasks.py
+++ tasks.py
@@ -1,12 +1,50 @@
 # FIle responsible for creating and modifying task objects
 
-class Tasks:
-    def __init__(self, request):
-        # request arg confirms what user wants to do (edit, remove, etc...)
-        self.__id = None # ID for requests
-        self.__description = None
-        self.__status = None
-        self.__created_at = None
-        self.__updated_at = None
-        self.request = request
+tasks = [] # This will store our tasks
 
+class Task:
+    def __init__(self, user_name):
+        self.name = user_name
+        self.task_list = [] # This will store our tasks
+
+        self.__task = {
+            "ID": None,
+            "Description": None, 
+            "Status": None,
+            "Created At": None,
+            "Updated At": None,
+        } # contains tasks created
+
+    def add_description(self, description):
+        self.__task["Description"] = description
+        # Append to task list
+
+    def update_description(self, description):
+        pass
+
+    def add_status(self, status):
+        pass
+
+    def update_status(self, status):
+        pass
+
+    def complete_task(self, id):
+        pass # Pass ID to auto complete task
+
+    def delete_task(self, id):
+        pass # Pass ID to delete task (require user to validate before deletion)
+
+    def get_tasks(self):
+        pass # Fetch a list of all tasks
+
+    def get_completed(self):
+        pass 
+
+    def get_uncompleted(self):
+        pass
+
+    def get_inprogress(self):
+        pass
+
+    def append_to_list(self):
+        pass # Appends to list when method is called

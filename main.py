48bd2802c3388c8504d271d796967fafc37df2bc
James Hatfield <james.hatfield17@icloud.com>
2025-06-01 13:42:01 +0100

    Imported program message, created initial loop for prompting user

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
 # File responsible for running project
+# Program will loop allow user to modify their list until an exit crtieria is met
+
+from tasks import Task
+from programMessage import header, body, footer
+
+active = True
+
+while active:
+    user_input = input("What would you like to do? ").lower()
+
+    match user_input:
+        case "exit":
+            active = False
+
+        case "add":
+            print("You will add an item")
+
+        case "get":
+            print("You will see all items")

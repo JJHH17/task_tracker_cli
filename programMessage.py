669d4bf2d79e264ffd3095940781fac0eba6cfeb
James Hatfield <james.hatfield17@icloud.com>
2025-06-01 13:47:53 +0100

    Tweaked formatting of message for placement

--- programMessage.py
+++ programMessage.py
@@ -1,11 +1,11 @@
 # File responsible for holding program welcome messaging
 
 header = print("""
-               ________________________________________\n
-               Welcome to this Task Manager application!\n
-               ________________________________________\n""")
+               _______________________________________________\n
+                Welcome to this Task Manager application!\n
+               _______________________________________________\n""")
 
-body = print("""Please enter the ID of the task you wish to modify\n
-             Use the "get_tasks" method to fetch a list of tasks\n""")
+body = print("""\t\tPlease enter the ID of the task you wish to modify\n
+                se the "get_tasks" method to fetch a list of tasks\n""")
 
-footer = print("_______________________________________")
+footer = print("______________________________________________")

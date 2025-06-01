75f1cde3314b3d626fc86d0b45dc7f274a525c93
James Hatfield <james.hatfield17@icloud.com>
2025-06-01 13:35:22 +0100

    Added header, body and footer message for program loop

---
+++ programMessage.py
@@ -0,0 +1,11 @@
+# File responsible for holding program welcome messaging
+
+header = print("""
+               ________________________________________\n
+               Welcome to this Task Manager application!\n
+               ________________________________________\n""")
+
+body = print("""Please enter the ID of the task you wish to modify\n
+             Use the "get_tasks" method to fetch a list of tasks\n""")
+
+footer = print("_______________________________________")

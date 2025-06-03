76638c5530b6958dd1b16ec45707b5bf9898816f
James Hatfield <james.hatfield17@icloud.com>
2025-06-03 09:52:37 +0100

    Added more file handling functions to write and read JSON files

--- fileHandler.py
+++ fileHandler.py
@@ -13,4 +13,17 @@ def check_file():
             pass  # Creates the file
         print("File created.")
 
+# Used to write to file
+def write_file(input):
+    with open(path, "w") as f:
+        f.write(input)
 
+# Used to read file
+def read_file():
+    with open(path, "r") as f:
+        print(f.read())
+
+# Used to append to file
+def append_file(input):
+    with open(path, "a") as f:
+        f.write(input)
